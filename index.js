228b7c8d156a51e323ee61429e60cc48ce693049
Liu Y Cheng <xsro@foxmail.com>
2022-03-03 23:42:20 +0800

    add configs

--- index.js
+++ index.js
@@ -4,68 +4,29 @@ import Clash from "./lib/clash.js";
 import { asAbsolutePath } from "./lib/util.js";
 import * as fs from "fs/promises";
 import { logger } from "./lib/logger.js";
-import { extname, resolve, isAbsolute, dirname } from "path";
+import { join, resolve } from "path";
 import YAML from "yaml";
 import { ips } from "./lib/ip.js";
 import genPAC from "./lib/pac.js";
 import { existsSync, readFileSync } from "fs";
 import { execSync } from "child_process";
+import { getConfig } from "./lib/getConfig.js";
+import { config, defaultConfig } from "./lib/local.config.js";
 
-async function getConfig(conf) {
-    let configPath = conf;
-    let configIdx = typeof conf === "string" && parseInt(conf) >= 0 ? parseInt(conf) : undefined;
-    if (typeof configIdx === "number") {
-        if (existsSync(asAbsolutePath("_config"))) {
-            const files = await fs.readdir(asAbsolutePath("_config/"));
-            const cfgs = files
-                .filter(file => !file.startsWith("_") && file.endsWith(".yml"))
-                .map(file => asAbsolutePath("_config/" + file));
-            logger.info("finded " + Object.entries(cfgs).map(val => { let [k, v] = val; return `${k} ${v}` }).join("\t\n"))
-            let i = conf ? parseInt(conf) : 0;
-            configPath = cfgs[i]
+program
+    .command("config [key] [value]")
+    .description("manage configs")
+    .option("-l,--list", "list the current config")
+    .option("-d,--list-default", "list default config")
+    .action(function (key, value, options) {
+        if (options.list) {
+            console.log("\t" + Object.entries(config).map(val => val[0] + ":\t" + val[1]).join("\n\t"))
         }
-        else {
-            if (conf === 1) {
-                configPath = asAbsolutePath("config/GreenFish.json")
-            }
-            if (conf === 2) {
-                configPath = asAbsolutePath("config/GreenFish.yml")
-            }
-            if (conf === 3) {
-                configPath = asAbsolutePath("config/SS-Rule-Snippet.yml")
-            }
+        if (options.listDefault) {
+            console.log("\t" + Object.entries(defaultConfig).map(val => val[0] + ":\t" + val[1]).join("\n\t"))
         }
-    }
-
-    if (!isAbsolute(configPath)) {
-        configPath = resolve(process.cwd(), configPath)
-    }
-    const configText = await fs.readFile(configPath, { encoding: "utf-8" });
-    let config = null
-    switch (extname(configPath).toLowerCase()) {
-        case ".yml":
-        case ".yaml":
-            config = YAML.parse(configText)
-            break;
-        case ".json":
-            config = JSON.parse(configText)
-            break
-    }
-    if (config === null) {
-        throw new Error("can't read config file")
-    }
-
-    const { parser } = config;
-    parser.destination = isAbsolute(parser.destination)
-        ? parser.destination
-        : resolve(dirname(configPath), parser.destination);
-    if (parser[process.platform]) {
-        for (const key in parser[process.platform]) {
-            parser[key] = parser[process.platform][key];
-        }
-    }
-    return { configPath, config }
-}
+        config[key] = value
+    })
 
 program
     .command("generate")
@@ -119,14 +80,8 @@ program
     .option("-d,--daemon <screen|pm2>", "use daemon to run clash")
     .option("-c,--clash-log <redirect|all|force>", "clash log")
     .option("-s,--secret [string]", "set secret for API")
-    .option("-l,--log-level [string]", "set log level: 0:clash输出   2:  3:软件执行进度  4:部分重要执行结果 5:连接日志")
     .action(
         async function (uclashProfile, options) {
-            if (options.logLevel === true) {
-                logger.level = 0
-            } else if (typeof options.logLevel === "string") {
-                logger.level = parseInt(options.logLevel)
-            }
             logger.info(1, options)
 
             if (existsSync(asAbsolutePath("_ui"))) {
