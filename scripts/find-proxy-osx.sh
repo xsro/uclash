90e4c5d4919528d9a5e7667cf7e59ee7a196d325
Liu Y Cheng <xsro@foxmail.com>
2022-03-14 20:42:00 +0800

    add bin for find proxy

--- scripts/find-proxy-osx.sh
+++ scripts/find-proxy-osx.sh
@@ -1,9 +1,11 @@
+#!/usr/bin/env sh
 proxy_ip=$(arp -a | grep -oE '192.168.[0-9]+.[0-9]+' | head -1)
 proxy_ui_port=9090
 proxy_pac_path="/ui/c/index.txt"
 logPath="/tmp/uclash.findproxy.log"
 url="http://$proxy_ip:$proxy_ui_port$proxy_pac_path"
 rm $logPath
+echo "checking $proxy_ip"
 curl $url -o $logPath
 echo "downloaded $url"
 
