349a95c62ef2fc346400ea30d0827c490b97d5b7
Liu Y Cheng <xsro@foxmail.com>
2022-07-17 15:26:05 +0800

    fix script bug

--- bin/cft.sh
+++ bin/cft.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 current_path=$(readlink $0)
-if [ "$current_path"a == ""a ]; then
-    current_path=$0
+if [ -z $current_path ]
+then current_path=$0
 fi
 project_dir=$(dirname $current_path)
 project_dir=$(dirname $project_dir)
@@ -17,12 +17,12 @@ case $1 in
         bash $project_dir/scripts/setup-in-termux.sh
         ;;
     r|reload)
-        if [ -n $3 ]
-        then controller=$3
-        else controller="127.0.0.1:9090"
-        fi
         file=$2
-        curl -X PUT http://$controller/configs \
+        clash_controller=$3
+        if [ -z $3 ]
+        then clash_controller="127.0.0.1:9090"
+        fi
+        curl -X PUT http://$clash_controller/configs \
             -H "Content-Type:application/json" \
             -d "{\"path\":\"$file\"}"
         ;;
