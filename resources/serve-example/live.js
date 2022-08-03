9e84b5ab674e6a6718d3ea7d3b7450f231ddee6b
Liu Y Cheng <xsro@foxmail.com>
2022-08-04 00:21:22 +0800

    fix github action

--- resources/serve-example/live.js
+++ resources/serve-example/live.js
@@ -1,4 +1,4 @@
 module.exports = function (req, res) {
-    const { fs } = this.preloaded
+    const { fs, path } = this.preloaded
     res.end(fs.readFileSync(path.resolve(__dirname, "live.html"), "utf-8"))
 }
