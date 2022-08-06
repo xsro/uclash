79de2be6ef0301feb8cefba3d44a9a67b6c57654
Liu Y Cheng <xsro@foxmail.com>
2022-08-06 11:07:04 +0800

    pac: redirect clash to my clash server

--- resources/serve-hook-example/pac.js
+++ resources/serve-hook-example/pac.js
@@ -9,8 +9,7 @@ async function hook(req, res) {
     const pacs = config.get("pacs");
     const cacheOpt = config.get("pacs-cache");
 
-    async function genPAC(pacname, ip, profileObj = { port: 7890, "socks-port": 7891 }) {
-        const pac = pacs.find(p => p.name = pacname)
+    async function genPAC(pac, ip, profileObj = { port: 7890, "socks-port": 7891 }) {
         if (!pac) return undefined
 
         let output = "";
@@ -18,7 +17,14 @@ async function hook(req, res) {
             ? readFileSync(resolve(paths.projectFolder, pac.base), "utf-8")
             : await download2(pac.base, cacheOpt).catch(console.error)
         if (template) {
-            output = template.replace(pac.replace, pac.to)
+            if (Array.isArray(pac.replace)) {
+                output = template
+                for (const [from, to] of pac.replace) {
+                    output = output.replace(from, to)
+                }
+            } else {
+                output = template.replace(pac.replace, pac.to)
+            }
             output = output
                 .replace(/\@ip/g, ip)
                 .replace(/\@port/g, profileObj["port"])
@@ -28,9 +34,14 @@ async function hook(req, res) {
     }
     if (reqUrl.pathname.endsWith(".pac")) {
         res.writeHead(200, { "Content-Type": "text/plain" });
-        const pacName = reqUrl.pathname.replace(".pac", "").replace("/", "")
-        const pacText = await genPAC(pacName, ip, undefined)
-        res.end("//" + pacName + "\t" + new Date().toLocaleString() + "\n" + pacText)
+        const pacName = reqUrl.pathname.replace("/", "")
+        const pac = pacs.find(p => p.name === pacName)
+        const pacText = await genPAC(pac, ip, undefined)
+        const head = `
+// ${pacName} ${new Date().toLocaleString()}
+// base: ${pac.base}
+`
+        res.end(head + "\n" + pacText)
         this.prevent()
     }
 }
