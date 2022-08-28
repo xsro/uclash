732c9934792eee57c296e440e153d219a3139b54
Liu Y Cheng <xsro@foxmail.com>
2022-08-28 23:30:33 +0800

    fix

--- src/config.ts
+++ src/config.ts
@@ -4,7 +4,7 @@ import { curlCp } from "./util/curl"
 
 export const config = new Config()
 config.curl = new curlCp(config.get("curl", {}))
-config.cURL = config.curl.cURL
+config.cURL = config.curl.cURL.bind(config.curl)
 
 export default config
 
