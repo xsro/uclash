757024ac0b20f95f9b10ff39eb1783a193ea46cd
Liu Y Cheng <xsro@foxmail.com>
2022-09-03 01:09:50 +0800

    uc find sort

--- scripts/find-proxy.sh
+++ scripts/find-proxy.sh
@@ -1,18 +1,20 @@
 #!/usr/bin/env sh
-proxy_ips=$(arp -a | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
-proxy_ui_port=9090
+# xsro@foxmail.com
+# find proxy in arp and history
 TMPDIR=$PREFIX/tmp
 previous_path=$HOME/.config/uclash/previous_proxy.txt
+totry_path=$HOME/.config/uclash/totest.txt
+
 #https://stackoverflow.com/questions/5947742/how-to-change-the-output-color-of-echo-in-linux/28938235#28938235
 green='\033[0;32m'
 red='\033[0;31m'
 NC='\033[0m' # No Color
 
 function req(){
-    url=$1
-    proxy=$2
-    name=$3
-    FILE="$TMPDIR/uclash.findproxy.$name.html"
+    local url=$1
+    local proxy=$2
+    local name=$3
+    local FILE="$TMPDIR/uclash.findproxy.$name.html"
     if [ -f $FILE ]
     then rm $FILE
     fi
@@ -31,29 +33,46 @@ function req(){
 }
 
 function test_proxy(){
-    proxy=$1
-    printf "try $proxy\t"
+    local proxy=$1
     if req www.baidu.com $proxy baidu
     then 
-        check_ip=$(node $(dirname $0)/../bin/uclash ip $proxy -p myip -k location.country_code | tail -1)
+        check_ip=$(node $(dirname $0)/../bin/uclash ip $proxy -p myip -k location.country_code | tail -1 2>/dev/null)
         printf "[location:${check_ip}]"
+        return 0
     fi
-    printf "\n"
+    return 1
 }
 
-for proxy_ip in $proxy_ips
+echo "">$totry_path
+function appendline(){
+    local line=$1
+    echo "$line" >>$totry_path
+}
+for proxy_ip in $(arp -a | grep -oE '[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+')
 do 
-    test_proxy   http://$proxy_ip:7890  
-    test_proxy   socks://$proxy_ip:7891 
+    appendline http://$proxy_ip:7890
+    appendline socks://$proxy_ip:7891
 done
 
-echo "==testing $previous_path"
 if [ -f "$previous_path" ]
 then
-    sort  $previous_path | uniq - $previous_path.uniq
-    for proxy in $(cat $previous_path.uniq)
-    do test_proxy $proxy norec
-    done
-    mv $previous_path.uniq $previous_path
+    cat $previous_path >>$totry_path
 fi
+
+for proxy in $(sort $totry_path -u )
+do
+    printf "try $proxy\t"
+    if [ "$1" = "-d" ]
+    then echo "\tdry run"
+    else
+        #https://stackoverflow.com/questions/11287861/how-to-check-if-a-file-contains-a-specific-string-using-bash
+        if (test_proxy $proxy) && !(grep -q "$proxy" "$previous_path")
+        then echo $proxy >> $previous_path
+        fi
+        echo ""
+    fi
+done
+
+
+
 
