d5c6b5da18a26f635a951f0e75abe72f0453bf16
Liu Y Cheng <xsro@foxmail.com>
2022-09-12 12:35:03 +0800

    确保config.curl注入

--- src/api.ts
+++ src/api.ts
@@ -5,6 +5,10 @@ import { systemIp, systemIp192, publicIP, getPublicIpProviders } from "./util/ip
 import { getAppProfiles, getAppProfile, ProfileInfo } from "./profile";
 import { ProfileType } from "./profile.def";
 import { ClashDashBoards } from "./util/default";
+import { curlCp } from "./util/curl"
+
+config.curl = new curlCp(config.get("curl", {}))
+config.cURL = config.curl.cURL.bind(config.curl)
 
 export {
      expr,
--- src/config.ts
+++ src/config.ts
@@ -1,10 +1,6 @@
 import { Config } from "./util/config"
-import { curlCp } from "./util/curl"
-
 
 export const config = new Config()
-config.curl = new curlCp(config.get("curl", {}))
-config.cURL = config.curl.cURL.bind(config.curl)
 
 export default config
 
